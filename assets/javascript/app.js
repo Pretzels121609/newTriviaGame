cf4a81eae034782107a56e09206c44bef3a2e4be
Kathy <Kathy@Kathy-Giebelhausens-iMac.local>
2018-03-03 00:50:13 -0700

    add javascript file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,94 @@
+//make a list of all the questions that will be used in the game
+//make a list of all the answers to each question
+//need buttons for each answer
+//start button to start the game
+//need a clock timer that counts down the time on each question
+//need a function to determine if the answer is correct or not from user
+//display to user if they got the question correct or not
+//if no user imput, game moves on to the next question
+//when there's no more questions game displays 'game over' over something to that effect
+//at end of game it shows you all the correct, incorrect and unanswered questions 
+//start over button that restarts the game
+
+
+var gameQandA = [
+    {
+        q: "Who invented the wonderful candy known as Peeps?",
+        o: ["Julia Childs", "Thom Fosse", "Unknown", "Sam Born"],
+        a: "Sam Born"
+    },
+
+    {
+        q: "In what city did Peeps first appear?",
+        o: ["Seattle", "Boston", "New York City", "St. Louis"],
+        a: "New York City"
+    },
+    
+    {
+        q: "How long does it take to make a Peep?",
+        o: ["12 hours", "1 hour", "6 minutes", "30 minutes"],
+        a: "6 minutes"
+    },
+
+    {
+        q: "A single Peep has how many calories?",
+        o: ["10", "27", "50", "32"],
+        a: "32"
+    },
+    {
+        q: "How many Peeps are made each DAY in the Bethlehem, Pennsylvania factory?",
+        o: ["4.2 million", "Too many to count!", "1 million", "500,000"],
+        a: "4.2 million"
+    }   
+]
+var answers = [null, null, null, null, null]
+var timer;
+
+$(document).ready(function(){
+    console.log("hello") 
+    $("#start").on("click", function() {
+        $("#start").addClass("hide")
+        $("#gameContent, #timer").removeClass("hide")
+        startTimer()
+    })
+    // $("#questionsAndAnswers").text("hello")
+    $(document).on("click",".option", function(){
+      console.log("click me")
+
+        var selectedButton = $(this).text()
+       
+        console.log(selectedButton)
+
+        // groupAttribute is the current button clicked's data-QAgroup 
+        // the value of data-QAgroup is always a integer
+        // use the interger as a position (aka as index) in the answers array
+        var groupAttribute = $(this).attr("data-QAgroup")
+
+        // find any button with the attribute data-QAgroup 
+        // the value of QAgroup needs to be the value of the attribute of the button clicked which is groupAtrribute
+        // when the buttons with the same attibute value are found remove the class selected
+        //and then add it just to the button thatb was recently clicked
+        $(`button[data-QAgroup="${groupAttribute}"]`).removeClass("selected");
+        $(this).addClass("selected")
+
+        console.log(groupAttribute)
+
+
+        // update the place holder (null) at the position which is groupAttribute
+        // replace 1 item at that position with the text from the current button clicked which is selectedButton
+        answers.splice(groupAttribute,1,selectedButton)
+        console.log(answers) 
+    }) 
+    $("#done").on("click", function(){
+        clearInterval(timer);
+        $("#gameContent").empty()
+        finalScore();
+    })  
+    
+    renderQandA() 
+    
+})
+
 //this function makes the question <p> tag and the options <btn>
 
 function renderQandA(){
